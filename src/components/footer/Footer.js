053531448aad9d502b850a59149692570e5981cf
RKKon <rkkon@mail.ru>
2022-08-21 20:28:10 +0300

    update link to repo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="footer-copyright">
         <div className="container">
         © {new Date().getFullYear()} Copyright Text
-        <a className="grey-text text-lighten-4 right" href="#">My Repository</a>
+        <a target='_blank' rel="noreferrer" className="grey-text text-lighten-4 right" href="https://github.com/RKKon/React-app-mini-store/tree/main">My Repository</a>
         </div>
       </div>
     </footer>
